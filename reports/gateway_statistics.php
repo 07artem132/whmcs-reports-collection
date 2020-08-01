2783bc17803916b543bc9358b9a160d2cb883c77
Artem Ivanko <ivankoartem@gmail.com>
2020-08-02 00:57:04 +0300

    Update gateway_statistics.php

--- reports/gateway_statistics.php
+++ reports/gateway_statistics.php
@@ -10,11 +10,14 @@ $reportdata["title"] = "Популярные платежные шлюзы";
 $reportdata["description"] = "Рейтинг популярных платежных шлюзов";
 
 $reportdata["tableheadings"] = array("Название шлюза", "количество");
-$results = Transaction::select(Capsule::raw('count(*) as count,gateway'))->groupBy('gateway')->get();
-foreach ($results as $result)
-{
-   $reportdata["tablevalues"][] = array($result['gateway'], $result['count']);
-    $chartdata['rows'][] = ['c' => array(array('v' => $result['gateway']), array('v' => $result['count']))];
+$pmonth = str_pad((int)$month, 2, "0", STR_PAD_LEFT);
+
+
+$results = Transaction::select(Capsule::raw('sum(amountin) as sum,gateway,date'))->where('date', 'LIKE', (int)$year . "-" . $pmonth . '-%')->groupBy('gateway')->get();
+$sql = Transaction::select(Capsule::raw('sum(amountin) as sum,gateway,date'))->where('date', 'LIKE', (int)$year . "-" . $pmonth . '-%')->groupBy('gateway')->toSql();
+foreach ($results as $result) {
+    $reportdata["tablevalues"][] = array($result['gateway'], $result['sum']);
+    $chartdata['rows'][] = ['c' => array(array('v' => $result['gateway']), array('v' => $result['sum']))];
 }
 
 $reportdata["footertext"] = "<p>* denotes converted to default currency</p>";
@@ -26,5 +29,6 @@ $args = array();
 $args['legendpos'] = 'right';
 
 $reportdata["headertext"] = $chart->drawChart('Pie', $chartdata, $args, '300px');
+$reportdata["monthspagination"] = true;
 
 ?>
